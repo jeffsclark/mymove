a60fd6a6b7ac15c78d9197c6e845e0f4283e09c9
Breanne Boland <breanne@truss.works>
2018-02-21 14:06:10 -0800

    adding jest.fn to try to offer two results from the mock api

--- src/scenes/SubmittedFeedback/__mocks__/api.js
+++ src/scenes/SubmittedFeedback/__mocks__/api.js
@@ -1,16 +1,40 @@
-export const IssuesIndex = () => {
-  console.log('Legit');
-  return new Promise(resolve => {
-    const results = [
-      {
-        created_at: '2018-02-14T12:39:32.919Z',
-        description: 'Moving is a difficult thing, alas',
-        id: 'f827b6bc-7a35-459c-8a44-aec602a80bab',
-        updated_at: '2018-02-14T12:39:32.919Z',
+export const IssuesIndex = jest
+  .fn() // =>
+  .mockImplementationOnce(
+    // return (
+    new Promise(
+      resolve => {
+        const results = [
+          {
+            created_at: '2018-02-14T12:39:32.919Z',
+            description: 'Moving is a difficult thing, alas',
+            id: 'f827b6bc-7a35-459c-8a44-aec602a80bab',
+            updated_at: '2018-02-14T12:39:32.919Z',
+          },
+        ];
+        process.nextTick(() => resolve(results));
       },
-    ];
-    process.nextTick(() => resolve(results));
-  });
-};
+      // )
+    ),
+  )
+  .mockImplementationOnce(console.log('heysecond'));
+// console.log('Legit')
+// return (
+// new Promise(resolve => {
+//   const results = [
+//     {
+//       created_at: '2018-02-14T12:39:32.919Z',
+//       description: 'Moving is a difficult thing, alas',
+//       id: 'f827b6bc-7a35-459c-8a44-aec602a80bab',
+//       updated_at: '2018-02-14T12:39:32.919Z',
+//     },
+//   ];
+//   process.nextTick(() => resolve(results));
+// )
+// The idea here is that when it's called a second time, it will ultimately return something that isn't a valid promise.
+// .mockImplementationOnce(() => console.log('other promise goes here'))
+// );
+// });
 // Give it a good and bad call so we can observe what happens
 // with both. Look up how to return a bad promise.
+// const getHashedPasswordFromDB = jest.fn(() => Promise.resolve({}))
--- src/scenes/SubmittedFeedback/ducks.test.js
+++ src/scenes/SubmittedFeedback/ducks.test.js
@@ -72,22 +72,15 @@ jest.mock('./api');
 
 // TODO: Figure out how to mock the Swagger API call
 describe('async action creators', () => {
-  it('should receive a valid promise and handle it properly', () => {
-    const testLoadIssues = loadIssues(); // Make two tests; name things accordingly depending on intent (success or failure)
-    const getState = function() {
-      return;
-    };
-    const dispatch = function(action) {
-      return;
-    };
+  const initialState = { issues: null, hasError: false };
+  const mockStore = configureStore();
+  let store;
 
-    testLoadIssues(dispatch, getState);
+  beforeEach(() => {
+    store = mockStore(initialState);
   });
 
-  it('should receive an invalid promise and handle it properly', () => {
-    // Need to get an invalid promise somehow - does that require another mock? Need to show that error.
-    // Am I testing the action value or the issue returned in the promise?
-    // If the promise value comes from the mock, am I testing for that? Should I provide the value to the mock function and then test for the provided value, rather than having the value hard-coded in the mock?
+  it('should receive a valid promise and handle it properly', () => {
     const testLoadIssues = loadIssues();
     const getState = function() {
       return;
@@ -96,30 +89,53 @@ describe('async action creators', () => {
       return;
     };
 
-    // testLoadIssues(dispatch, getState);
+    testLoadIssues(dispatch, getState);
     // return store.dispatch(loadIssues()).then(() => {
     //   // return of async actions
     //   expect(store.getActions()).toEqual(expectedActions);
+    // });
+
+    it('should receive an invalid promise and handle it properly', () => {
+      // Need to get an invalid promise somehow - does that require another mock? Need to show that error.
+      // Am I testing the action value or the issue returned in the promise?
+      // If the promise value comes from the mock, am I testing for that? Should I provide the value to the mock function and then test for the provided value, rather than having the value hard-coded in the mock?
+      // Make loadIssues available
+      // Provide mocked versions of what it needs to run (feel like current approach is anemic)
+      // Provide it a valid promise and test that result (expect items?)
+      // Separate it function: provide it an invalid promise and test that result (expect error?)
+      const testLoadIssues = loadIssues();
+      // const getState = function() {
+      //   return;
+      // };
+      // const dispatch = function(action) {
+      //   return;
+      // };
+
+      // testLoadIssues(dispatch, getState);
+      // return store.dispatch(loadIssues()).then(() => {
+      //   // return of async actions
+      //   expect(store.getActions()).toEqual(expectedActions);
+    });
+    // it('creates SHOW_ISSUES_SUCCESS when submitted issues have been loaded', () => {
+    //   fetchMock.getOnce('/submitted', {
+    //     items: { issues: [{ id: 11, description: 'too few dogs' }] },
+    //     headers: { 'content-type': 'application/json' },
+    //   });
+
+    //   const expectedActions = [
+    //     { type: SHOW_ISSUES },
+    //     {
+    //       type: SHOW_ISSUES_SUCCESS,
+    //       items: { issues: [{ id: 11, description: 'too few dogs' }] },
+    //     },
+    //   ];
+
+    //   const store = mockStore(initialState);
+
+    //   return store.dispatch(loadIssues()).then(() => {
+    //     // return of async actions
+    //     expect(store.getActions()).toEqual(expectedActions);
+    //   });
+    // });
   });
-  // it('creates SHOW_ISSUES_SUCCESS when submitted issues have been loaded', () => {
-  //   fetchMock.getOnce('/submitted', {
-  //     items: { issues: [{ id: 11, description: 'too few dogs' }] },
-  //     headers: { 'content-type': 'application/json' },
-  //   });
-
-  //   const expectedActions = [
-  //     { type: SHOW_ISSUES },
-  //     {
-  //       type: SHOW_ISSUES_SUCCESS,
-  //       items: { issues: [{ id: 11, description: 'too few dogs' }] },
-  //     },
-  //   ];
-
-  //   const store = mockStore(initialState);
-
-  //   return store.dispatch(loadIssues()).then(() => {
-  //     // return of async actions
-  //     expect(store.getActions()).toEqual(expectedActions);
-  //   });
-  // });
 });
